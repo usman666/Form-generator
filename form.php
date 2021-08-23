c040caa7578f3e46663e1dae034abb450ebc25a4
Usman Ali <usmanali@Usmans-MacBook-Pro.local>
2021-08-23 19:50:40 +0100

    connect to usman

--- form.php
+++ form.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
 
-    $conn= mysqli_connect("localhost","root","","formGenerator");
+    $conn= mysqli_connect("eu-cdbr-west-01.cleardb.com","b53e8aca15beb0","bc48f174","heroku_8c0948aa4569c5c");
+
 
     if(isset($_GET['id'])) {
             $id = $_GET['id'];
